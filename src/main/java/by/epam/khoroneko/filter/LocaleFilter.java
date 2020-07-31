fc6bb192776801a15682ce9f84539ba5e13656cc
Alesya Khoroneko <horoneko00@gmail.com>
2020-07-31 23:16:41 +0300

    Fix request encoding

--- src/main/java/by/epam/khoroneko/filter/LocaleFilter.java
+++ src/main/java/by/epam/khoroneko/filter/LocaleFilter.java
@@ -6,17 +6,18 @@ import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.Console;
 import java.io.IOException;
 import java.util.Locale;
 
 @WebFilter(filterName = "LocaleFilter", urlPatterns = {"/*"}, dispatcherTypes = {DispatcherType.REQUEST})
 public class LocaleFilter extends HttpFilter {
     private static final String ENGLISH = "en";
+    private static final String ENCODING = "UTF-8";
 
 
     public void doFilter(ServletRequest request, ServletResponse response,
                          FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding(ENCODING);
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse resp = (HttpServletResponse) response;
         HttpSession session = req.getSession(true);
